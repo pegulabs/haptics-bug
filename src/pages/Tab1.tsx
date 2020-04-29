a6b8244c90763e075d6d441edc60ed1a1a9d4548
Aubrey Holland <aubreyholland@gmail.com>
2020-04-29 11:01:33 -0400

    whatever

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonRefresher, IonRefresherContent, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { RefresherEventDetail } from '@ionic/core';
 
 const Tab1: React.FC = () => {
+  function doRefresh(event: CustomEvent<RefresherEventDetail>) {
+    console.log('Begin async operation');
+  
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.detail.complete();
+    }, 2000);
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -12,6 +22,9 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
